470356b3b871704f05f43e27a4fcff02d4decc32
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-01-20 18:31:40 +0100

    Add user router

--- routers/ProjectRouter.ts
+++ routers/ProjectRouter.ts
@@ -3,20 +3,27 @@ import { FastifyInstance, FastifyRequest } from "fastify";
 import { prisma } from "..";
 
 export const projectRouter = (server: FastifyInstance, _: any, done: any) => {
-  server.get("/getallprojects", async () => {
-    try {
-      const data = await prisma.projects.findMany({
-        select: {
-          id: true,
-          title: true,
-          image: true,
-        },
-      });
-      return data;
-    } catch (error) {
-      console.log(error);
+  server.get(
+    "/getallprojects",
+    async (req: FastifyRequest<{ Body: string }>) => {
+      try {
+        const body = JSON.parse(req.body) as { ownerId: string };
+        const data = await prisma.projects.findMany({
+          where: {
+            ownerId: body.ownerId,
+          },
+          select: {
+            id: true,
+            title: true,
+            image: true,
+          },
+        });
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+  );
   server.get(
     "/getsingleproject/:id",
     async (req: FastifyRequest<{ Params: { id: string } }>) => {
@@ -29,10 +36,17 @@ export const projectRouter = (server: FastifyInstance, _: any, done: any) => {
     }
   );
 
-  server.post("/createproject", async () => {
-    const newProject = await prisma.projects.create({ data: {} });
-    return newProject;
-  });
+  server.post(
+    "/createproject",
+    async (req: FastifyRequest<{ Params: { ownerId: string } }>) => {
+      const newProject = await prisma.projects.create({
+        data: {
+          ownerId: req.params.ownerId,
+        },
+      });
+      return newProject;
+    }
+  );
   server.post(
     "/updateproject/:id",
     async (req: FastifyRequest<{ Params: { id: string }; Body: string }>) => {
---
+++ routers/UserRouter.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance, FastifyRequest } from "fastify";
+
+import { prisma } from "..";
+
+export const userRouter = (server: FastifyInstance, _: any, done: any) => {
+  server.post("/createuser", async (req: FastifyRequest<{ Body: string }>) => {
+    try {
+      const data = JSON.parse(req.body) as {
+        id: string;
+        nickname: string;
+        email: string;
+      };
+      await prisma.user.create({
+        data,
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  server.delete(
+    "/deleteuser",
+    async (req: FastifyRequest<{ Body: string }>) => {
+      try {
+        const data = JSON.parse(req.body) as {
+          id: string;
+        };
+        await prisma.user.delete({
+          where: {
+            id: data.id,
+          },
+        });
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+  done();
+};
