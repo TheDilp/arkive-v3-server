3e526cc2246657f317c945505329fa62a6bdbed5
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-01-20 18:40:47 +0100

    Add update route for user

--- routers/UserRouter.ts
+++ routers/UserRouter.ts
@@ -18,6 +18,21 @@ export const userRouter = (server: FastifyInstance, _: any, done: any) => {
       console.log(error);
     }
   });
+  server.post("/updateuser", async (req: FastifyRequest<{ Body: string }>) => {
+    try {
+      // const data = JSON.parse(req.body) as {
+      //   id: string;
+      //   nickname: string;
+      //   email: string;
+      // };
+      // await prisma.user.create({
+      //   data,
+      // });
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  });
   server.delete(
     "/deleteuser",
     async (req: FastifyRequest<{ Body: string }>) => {
