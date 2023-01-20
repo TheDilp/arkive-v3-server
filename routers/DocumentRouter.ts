e0b5d0762ddca050225ef859384b0283c959a3a4
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-01-21 00:10:14 +0100

    Change docRouter

--- routers/DocumentRouter.ts
+++ routers/DocumentRouter.ts
@@ -1,68 +1,90 @@
-import { FastifyInstance, FastifyRequest } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 import { prisma } from "..";
 import { removeNull } from "../utils/transform";
 
 export const documentRouter = (server: FastifyInstance, _: any, done: any) => {
-  server.get("/getalldocuments/:project_id", async (req: FastifyRequest<{ Params: { project_id: string } }>) => {
-    const data = await prisma.documents.findMany({
-      where: {
-        project_id: req.params.project_id,
-      },
-      select: {
-        id: true,
-        title: true,
-        icon: true,
-        parentId: true,
-        parent: {
-          select: {
-            id: true,
-            title: true,
+  server.get(
+    "/getalldocuments/:project_id",
+    async (req: FastifyRequest<{ Params: { project_id: string } }>) => {
+      const data = await prisma.documents.findMany({
+        where: {
+          project_id: req.params.project_id,
+          OR: [
+            {
+              project: {
+                ownerId: req.user_id,
+              },
+            },
+            {
+              project: {
+                members: {
+                  some: {
+                    id: req.user_id,
+                  },
+                },
+              },
+            },
+          ],
+        },
+        select: {
+          id: true,
+          title: true,
+          icon: true,
+          parentId: true,
+          parent: {
+            select: {
+              id: true,
+              title: true,
+            },
+          },
+          template: true,
+          folder: true,
+          sort: true,
+          expanded: true,
+          isPublic: true,
+          project_id: true,
+          alter_names: true,
+          image: true,
+          tags: {
+            select: {
+              id: true,
+              title: true,
+            },
           },
         },
-        template: true,
-        folder: true,
-        sort: true,
-        expanded: true,
-        isPublic: true,
-        project_id: true,
-        alter_names: true,
-        image: true,
-        tags: {
-          select: {
-            id: true,
-            title: true,
-          },
+        orderBy: {
+          sort: "asc",
         },
-      },
-      orderBy: {
-        sort: "asc",
-      },
-    });
-    return data;
-  });
+      });
+      return data;
+    }
+  );
 
-  server.get("/getsingledocument/:id", async (req: FastifyRequest<{ Params: { id: string } }>) => {
-    const doc = await prisma.documents.findUnique({
-      where: { id: req.params.id },
-      include: {
-        tags: {
-          select: {
-            id: true,
-            title: true,
+  server.get(
+    "/getsingledocument/:id",
+    async (req: FastifyRequest<{ Params: { id: string } }>) => {
+      const doc = await prisma.documents.findUnique({
+        where: { id: req.params.id },
+        include: {
+          tags: {
+            select: {
+              id: true,
+              title: true,
+            },
           },
         },
-      },
-    });
-    return doc;
-  });
+      });
+      return doc;
+    }
+  );
 
   server.post(
     "/createdocument",
     async (
       req: FastifyRequest<{
         Body: string;
-      }>,
+      }>
     ) => {
       try {
         const newDocument = await prisma.documents.create({
@@ -74,7 +96,7 @@ export const documentRouter = (server: FastifyInstance, _: any, done: any) => {
         console.log(error);
       }
       return null;
-    },
+    }
   );
   server.post(
     "/updatedocument/:id",
@@ -83,52 +105,57 @@ export const documentRouter = (server: FastifyInstance, _: any, done: any) => {
         Body: string;
         Params: { id: string };
       }>,
+      rep: FastifyReply
     ) => {
       try {
-        const newDocument = await prisma.documents.update({
+        await prisma.documents.update({
           data: removeNull(JSON.parse(req.body)) as any,
           where: { id: req.params.id },
         });
-        return newDocument;
+        return true;
       } catch (error) {
         console.log(error);
       }
       return null;
-    },
+    }
   );
-  server.post("/sortdocuments", async (req: FastifyRequest<{ Body: string }>) => {
-    const indexes: { id: string; parent: string; sort: number }[] = JSON.parse(req.body);
-    const updates = indexes.map((idx) =>
-      prisma.documents.update({
-        data: {
-          parentId: idx.parent,
-          sort: idx.sort,
-        },
-        where: { id: idx.id },
-      }),
-    );
-    await prisma.$transaction(updates);
-  });
+  server.post(
+    "/sortdocuments",
+    async (req: FastifyRequest<{ Body: string }>) => {
+      const indexes: { id: string; parent: string; sort: number }[] =
+        JSON.parse(req.body);
+      const updates = indexes.map((idx) =>
+        prisma.documents.update({
+          data: {
+            parentId: idx.parent,
+            sort: idx.sort,
+          },
+          where: { id: idx.id },
+        })
+      );
+      await prisma.$transaction(updates);
+    }
+  );
   server.delete(
     "/deletedocument/:id",
     async (
       req: FastifyRequest<{
         Params: { id: string };
-      }>,
+      }>
     ) => {
       await prisma.documents.delete({
         where: {
           id: req.params.id,
         },
       });
-    },
+    }
   );
   server.delete(
     "/deletemanydocuments",
     async (
       req: FastifyRequest<{
         Body: string;
-      }>,
+      }>
     ) => {
       const ids = JSON.parse(req.body) as string[];
       if (ids)
@@ -140,7 +167,7 @@ export const documentRouter = (server: FastifyInstance, _: any, done: any) => {
           },
         });
       return true;
-    },
+    }
   );
 
   done();
