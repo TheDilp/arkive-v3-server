5e9030f0e1db4b4fe3d961285bf7187eaeb7e90e
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-01-22 21:30:58 +0100

    Fix ScreenRouter

--- routers/ScreenRouter.ts
+++ routers/ScreenRouter.ts
@@ -4,11 +4,11 @@ import { prisma } from "..";
 
 export const screenRouter = (server: FastifyInstance, _: any, done: any) => {
   server.get(
-    "/getallscreenes/:project_id",
+    "/getallscreens/:project_id",
     async (req: FastifyRequest<{ Params: { project_id: string } }>) => {}
   ),
     server.get(
-      "/getsinglescreene/:project_id",
+      "/getsinglescreen/:project_id",
       async (req: FastifyRequest<{ Params: { project_id: string } }>) => {
         try {
           const screens = await prisma.screens.findMany({
@@ -39,15 +39,16 @@ export const screenRouter = (server: FastifyInstance, _: any, done: any) => {
       }
     ),
     server.post(
-      "/createscreene/:project_id",
-      async (
-        req: FastifyRequest<{ Params: { project_id: string }; Body: string }>
-      ) => {
-        const data = JSON.parse(req.body) as { title: string };
+      "/createscreen",
+      async (req: FastifyRequest<{ Body: string }>) => {
+        const data = JSON.parse(req.body) as {
+          title: string;
+          project_id: string;
+        };
         await prisma.screens.create({
           data: {
             title: data.title,
-            project_id: req.params.project_id,
+            project_id: data.project_id,
           },
         });
       }
