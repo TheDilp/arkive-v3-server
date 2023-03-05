281c5216c09f45fb31d3c8d4c3c478b021c524bb
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-03-05 12:07:58 +0100

    Add router for timelines

---
+++ routers/TimelineRouter.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { prisma } from "..";
+
+export const calendarRouter = (server: FastifyInstance, _: any, done: any) => {
+  server.get(
+    "/getalltimelines/:project_id",
+    async (
+      req: FastifyRequest<{ Params: { project_id: string } }>,
+      rep: FastifyReply
+    ) => {
+      try {
+        const timelines = await prisma.timelines.findMany({
+          where: {
+            project_id: req.params.project_id,
+          },
+          orderBy: {
+            sort: "asc",
+          },
+        });
+        return timelines;
+      } catch (error) {
+        rep.status(500);
+        console.log(error);
+        return false;
+      }
+    }
+  );
+  server.post(
+    "/getsingletimeline",
+    async (req: FastifyRequest<{ Body: string }>, rep: FastifyReply) => {
+      try {
+        const data = JSON.parse(req.body) as { id: string };
+        const timeline = await prisma.timelines.findUnique({
+          where: {
+            id: data.id,
+          },
+        });
+        return timeline;
+      } catch (error) {
+        rep.status(500);
+        console.log(error);
+        return false;
+      }
+    }
+  );
+  done();
+};
