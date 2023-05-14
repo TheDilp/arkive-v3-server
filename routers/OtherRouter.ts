43c27f54d0aab121983d2acbd3b7a4aa61d2b28b
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-05-14 15:43:00 +0200

    chore: remove import

--- routers/OtherRouter.ts
+++ routers/OtherRouter.ts
@@ -1,5 +1,4 @@
 import { FastifyInstance, FastifyReply } from "fastify";
-import { createTransport } from "nodemailer";
 export const otherRouter = (server: FastifyInstance, _: any, done: any) => {
   server.get("/health_check", async (_, rep: FastifyReply) => {
     rep.code(200).send("OK");
