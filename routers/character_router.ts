ab1d6309b42bccf14060c9d5456f0b83d226fc2d
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-07-06 17:06:33 +0200

    feat: list all characters for project

--- routers/character_router.ts
+++ routers/character_router.ts
@@ -2,6 +2,7 @@ import { FastifyInstance, FastifyRegister, FastifyRequest } from "fastify";
 import { db, insertCharacterSchema } from "../utils";
 import { characters } from "../drizzle/schema";
 import { ResponseEnum } from "../enums/ResponseEnums";
+import { eq } from "drizzle-orm";
 
 export const characterRouter = (server: FastifyInstance, _: any, done: any) => {
   // #region create_routes
@@ -19,5 +20,25 @@ export const characterRouter = (server: FastifyInstance, _: any, done: any) => {
     }
   );
   // #endregion create_routes
+
+  // #region read_routes
+  server.get(
+    "/:projectId",
+    async (req: FastifyRequest<{ Params: { projectId: string } }>, rep) => {
+      const data = await db
+        .select({
+          id: characters.id,
+          firstName: characters.firstName,
+          lastName: characters.lastName,
+          nickname: characters.nickname,
+          age: characters.age,
+          imageId: characters.imageId,
+        })
+        .from(characters)
+        .where(eq(characters.projectId, req.params.projectId));
+      rep.send({ data, message: ResponseEnum.generic, ok: true });
+    }
+  );
+  // #endregion read_routes
   done();
 };
